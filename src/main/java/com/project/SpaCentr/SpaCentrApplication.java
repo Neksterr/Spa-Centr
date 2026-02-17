7f6bc434fb14d6248df52b1ecdbc2d04a81990e5
Nekster <nekster187@gmail.com>
2026-02-18 00:53:09 +0200

    spring boot

---
+++ src/main/java/com/project/SpaCentr/SpaCentrApplication.java
@@ -0,0 +1,13 @@
+package com.project.SpaCentr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaCentrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaCentrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/SpaCentr/SpaCentrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SpaCentr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaCentrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
